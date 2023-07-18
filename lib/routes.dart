638eca21a338b7c24c1c5f87d9fa7c9818d3febe
Az-21 <39803478+Az-21@users.noreply.github.com>
2023-07-18 13:26:57 +0530

    Add android page route

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,6 +1,9 @@
 import 'package:go_router/go_router.dart';
+import 'package:reccr/android/android_page.dart';
 import 'package:reccr/home/home_page.dart';
 
 final router = GoRouter(routes: [
-  GoRoute(path: "/", builder: (context, state) => const HomePage(), routes: []),
+  GoRoute(path: "/", builder: (context, state) => const HomePage(), routes: [
+    GoRoute(path: "android", builder: (context, state) => const AndroidPage()),
+  ]),
 ]);
