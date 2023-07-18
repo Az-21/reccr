db02e16f58427ead7559af7dc8955683efd4d417
Az-21 <39803478+Az-21@users.noreply.github.com>
2023-07-18 13:40:49 +0530

    Add monetization tiers

---
+++ lib/android/model.dart
@@ -0,0 +1,7 @@
+enum MonetizationEnum {
+  free, // Ad-free
+  premiumPlus, // Ad-free and reasonable free tier
+  premiumMinus, // Ad-free and annoying free tier
+  ads, // Ad supported
+  paid, // Fully paid
+}
